ebc3a147e4e17ce0ba93ced3eab3abf5ebb018e5
mikefab <mikefabrikant@gmail.com>
2017-04-12 02:38:09 -0400

    remove dubug

--- src/components/colorByRadioGroup.js
+++ src/components/colorByRadioGroup.js
@@ -35,7 +35,6 @@ class ColorByRadioGroup extends Component {
     var side_style = this.props.side_style;
     var style = {width:'250px'}
     return  <div >
-      aa
       <Grid style={style}>
         <Row className="show-grid">
           <Col xs={12} md={6}><input type="radio" checked={colorBy === "population"} onChange={this.setColorBy} value="population" /> population</Col>
